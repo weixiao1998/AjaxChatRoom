2503a289ae02b1426d9356d461e88bedc26c70b6
JiangJian1998 <1197749258@qq.com>
2019-12-21 13:19:05 +0800

    日常更新

--- backend/main.js
+++ backend/main.js
@@ -1,17 +1,14 @@
-//Node中无需指定package
-
-//导入其他的包
 let mysql = require('mysql')
 
 //使用第三方包提供的函数和对象
 //创建数据库连接池
 let pool = mysql.createPool({
-    host:"localhost",
-    port:"3306",
-    user:"root",
-    password:"123456",
-    database:"chatroom_db",
-    connectionLimit:100  //连接池大小限制
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "123456",
+    database: "chatroom_db",
+    connectionLimit: 100  //连接池大小限制
 })
 
 /*
@@ -23,58 +20,114 @@ console.log(result)
 })
 console.log('脚本执行完毕')
 */
-   
+
 //导入第三方模块：express，创建基于node.js的web服务器
 let express = require('express')
-
-//调用第三方模块提供的功能
 let server = express()
 
 //运行web服务器监听端口
 let port = 5050
-server.listen(port,()=>{
-    console.log("服务器启动成功！正在监听端口：",port)
+server.listen(port, () => {
+    console.log("服务器启动成功！正在监听端口：", port)
 })
 
 
-server.post('/user/register',(req,res)=>{
+/********************************************** */
+/*********************后台API****************** */
+/********************************************** */
+//使用Express提供的中间件：处理POST请求中的主体数据，保存在req.body属性中
+//处理application/x-www-form-urlencoded类型的请求数据
+server.use(express.urlencoded({
+    extended: false     //是否使用扩展工具解析请求主体
+}))
+//自定义中间件：允许指定客户端的跨域访问
+server.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*')  //当前服务器允许来自任何客户端的访问
+    next()  //放行，让后续的请求处理方法继续处理
+})
+
+server.post('/user/register', (req, res) => {
     //读取客户端提交的请求数据
-    let uname = req.body.uname
-    let nickname = req.body.nickname2
-    let upwd = req.body.upwd
+    let username = req.body.username
+    let nickname = req.body.nickname
+    let password = req.body.password
     let repwd = req.body.repwd
     let sex = req.body.sex
-    let age = req.body.age
+    let age = Number(req.body.age)
     let hobby = req.body.hobby
-    if(!uname){
-        res.json({code:401,msg:'uname required'})
+    if (!username) {
+        res.json({ code: 401, msg: '需要填写用户名' })
         return
     }
-    if(!upwd){
-        res.json({code:402,msg:'upwd required'})
+    if (!password) {
+        res.json({ code: 402, msg: '需要填写密码' })
         return
     }
-    if(!nickname){
-        res.json({code:403,msg:'nickname required'})
+    if (!nickname) {
+        res.json({ code: 403, msg: '需要填写昵称' })
         return
     }
-    if(!phone){
-        res.json({code:404,msg:'phone required'})
+    if (!sex) {
+        res.json({ code: 404, msg: '需要填写性别' })
         return
     }
+    if (!age) {
+        res.json({ code: 405, msg: '需要填写年龄' })
+        return
+    }
+
+    if (password != repwd) {
+        res.json({ code: 450, msg: '两次输入的密码不一致！' })
+        return
+    }
+    if (age < 1 || age > 150) {
+        res.json({ code: 451, msg: '年龄有误！' })
+        return
+    }
+
     //执行数据库操作-SELECT
-    let sql = 'SELECT uid FROM xz_user WHERE uname=? OR email=? OR phone=?'
-    pool.query(sql,[uname,email,phone],(err,result)=>{
-        if(err) throw err
-        if(result.length>0){
-            res.json({code:500, msg:'user already exists'})
+    let sql = 'SELECT id FROM cr_user WHERE username=?'
+    pool.query(sql, [username], (err, result) => {
+        if (err) throw err
+        if (result.length > 0) {
+            res.json({ code: 500, msg: '用户名已经存在！' })
             return
         }
         //执行数据库操作-INSERT
-        sql = 'INSERT INTO xz_user(uname,upwd,email,phone) VALUES (?,?,?,?)'
-        pool.query(sql,[uname,upwd,email,phone],(err,result)=>{
-            if(err) throw err
-            res.json({code:200, msg:'register succ', uid:result.insertId})
+        sql = 'INSERT INTO cr_user(username,nickname,password,sex,age,hobby) VALUES (?,?,?,?,?,?)'
+        pool.query(sql, [username, nickname, password, sex, age, hobby], (err, result) => {
+            if (err) throw err
+            res.json({ code: 200, msg: '注册成功!', id: result.insertId })
+            console.log(`${username} 注册成功！`)
         })
+    })
+})
+
+server.post('/user/login', (req, res) => {
+    let username = req.body.username
+    let password = req.body.password
+
+    if (!username) {
+        res.json({ code: 401, msg: '需要填写用户名' })
+        return
+    }
+    if (!password) {
+        res.json({ code: 402, msg: '需要填写密码' })
+        return
+    }
+
+    //执行数据库操作-SELECT
+    let sql = 'SELECT * FROM cr_user WHERE username=? AND password=?'
+    pool.query(sql, [username, password], (err, result) => {
+        if (err) throw err
+        console.log(result)
+        if (result.length > 0) {
+            res.json({ code: 200, msg: '登录成功！', user: result[0] })
+            return
+        } else {
+            res.json({ code: 400, msg: '用户名或密码错误！' })
+            return
+        }
+
     })
 })
